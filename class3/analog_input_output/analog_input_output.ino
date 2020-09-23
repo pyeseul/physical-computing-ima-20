d3691b0cce25120cf5151be172657b35fe15c68e
Yeseul Song <pyeseul@gmail.com>
2020-09-22 22:25:40 -0400

    Create analog_input_output.ino

---
+++ class3/analog_input_output/analog_input_output.ino
@@ -0,0 +1,26 @@
+/* This code reads analog values from a potentiometer connected to A0, 
+ * prints it out to the serial monitor, and change the brightness of an LED connected to Pin2.
+ */
+
+const int sensorPin = A0;
+const int ledPin = 2;  
+  
+int sensorValue = 0;
+int sensorValue_mapped = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {  
+  sensorValue = analogRead(sensorPin);
+  sensorValue_mapped = map(sensorValue, 0, 1023, 0, 255);
+  analogWrite(ledPin,sensorValue_mapped);
+
+  Serial.print(sensorValue);
+  Serial.print(" , ");
+  Serial.println(sensorValue_mapped);
+  
+  delay(20);
+}
